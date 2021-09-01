b60f5af91d481d6bf4806a000e22bdcec2386ae8
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-09-01 09:40:31 +0200

    generate random number

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+//Elementos sobre los que se va a trabajar
+const input = document.querySelector('.js-input');
+const button = document.querySelector('.js-button');
+const help = document.querySelector('.js-help');
+const tryNumber = document.querySelector('.js-try');
+
+//Generar un número Aleatorio y pintarlo en consola
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+console.log('Mi número aleatorio es:'getRandomNumber(100));
+
+
+
+
+//Llamar a la función
+button.addEventListener('click', getRandomNumber)
